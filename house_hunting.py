60237fd4db9dfd988734ac1855d7fe86cdec7d45
Tracy Falba <tfalba@mac.com>
2020-12-07 15:22:06 -0500

    first round of homework -- could add error checks

--- house_hunting.py
+++ house_hunting.py
@@ -1,1 +1,15 @@
-# Write your code here
+# These are given at start
+current_savings = 0
+r = .04
+portion_down_payment = .25
+
+annual_salary = int(input('Enter your annual salary: '))
+portion_saved = float(input('Enter the percent of your salary to save, as a decimal: '))
+total_cost = int(input('Enter the cost of your dream home: '))
+
+count = 0
+while current_savings < total_cost*portion_down_payment:
+  count += 1
+  current_savings += (current_savings*r)/12 + portion_saved*(annual_salary/12)
+
+print('Number of months: ' + str(count))
